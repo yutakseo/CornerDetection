800abfe34632b103e61fa8c94923835dd7c5c162
Yutak_Seo <soano2428@gmail.com>
2023-07-15 23:07:31 +0900

    delete

--- num.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-from test_dataset import *
-
-
-arr = np.array(test_data1)
-print(arr)
--- test.py
+++
@@ -1,5 +0,0 @@
-
-try:
-    print(10/0)
-except ZeroDivisionError as e:
-    e = print("v")
