98e3860ca787d9cda1535d5ee422317f19f1e50d
Yutak_Seo <soano2428@gmail.com>
2023-08-04 16:07:04 +0900

    23.08.04 행렬시각화 및 코너탐지코드 작성

---
+++ array_maker.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def make_array(data):
+    matrix = np.array(data)
+    return matrix
+
+def visual_array(data):
+    matrix = np.array(data)
+    rows, cols = matrix.shape
+    #사용할 컬러는 회색과 흰색
+    cmap_custom = plt.cm.colors.ListedColormap(['gray', 'white'])
+
+    #행렬을 이미지(그래프)로 표시
+    plt.imshow(matrix, cmap=cmap_custom, interpolation='nearest', extent=[0, cols, rows, 0], origin='upper')
+    plt.title("Matrix")
+    
+    #격자를 설정, 간격은 1
+    plt.xticks(np.arange(0, cols, step=1))
+    plt.yticks(np.arange(0, rows, step=1))
+    #x축을 상단에 고정
+    plt.tick_params(axis='x', which='both', bottom=False, top=True, labelbottom=False, labeltop=True)
+    #격자의 스타일
+    plt.grid(which='both', color='black', linestyle='-', linewidth=0.5)
+    
+    plt.show()
+
+    
--- cvCornerDetection.py
+++ cvCornerDetection.py
@@ -1,33 +1,27 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+from array_maker import *
 from test_dataset import *
 
-src = np.array(test_data11, dtype=np.uint8)
+img =test_data4
 
-corners = cv2.goodFeaturesToTrack(src, 3, 0.01, 10)
-corners = np.int0(corners)
+matrix = np.array(img, dtype=np.uint8)
 
+corner = cv2.cornerHarris(matrix, 2, 3, 0.04)
+coord = np.where(corner > 0.1* corner.max())
+coord = np.stack((coord[1], coord[0]), axis=-1)
 
+for x, y in coord:
+    cv2.circle(img, (x,y), 5, (0,0,255), 1, cv2.LINE_AA)
 
+corner_norm = cv2.normalize(corner, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
 
-for i in corners:
-    x, y = i.ravel()
-    cv2.circle(src, (x,y), 3, 255, -1)
-    
-plt.imshow(src)
-plt.show()
-    
+corner_norm = cv2.cvtColor(corner_norm, cv2.COLOR_GRAY2BGR)
+#merged = np.hstack((corner_norm, img))
 
+# 이미지를 화면에 표시
+cv2.imshow('Gray Image', corner)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-'''
-corner = cv2.cornerHarris(src, 5, 3, 0.3)
-ret, dst = cv2.threshold(corner, 0.1*corner.max(), 255, 0)
-dst = np.uint8(dst)
-
-ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-corners = cv2.cornerSubPix(corner,np.float32(centroids),(5,5),(-1,-1),criteria)
-for i in range(1, len(corners)):
-    print(corners[i])
-'''
+#visual_array(corners)
