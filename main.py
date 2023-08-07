05803fa758497b3a87aef1bc7ad64fb81978ff8c
yutak <soano2428@gmail.com>
2023-08-07 12:48:32 +0900

    23.08.07

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ from ribosome_module import ribosome
 from test_dataset import *
 from array_maker import visual_array
 
+
 visual_array(ribosome(test_data4))
+
+'''
+visual_array(src)
+visual_array(corner1)
+
+plt.imshow(corner1, cmap='jet')  # jet colormap을 사용하여 색상을 조정
+plt.colorbar()
+plt.show()'''
