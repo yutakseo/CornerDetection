b1342b14b87685c23fd7e935eccbb4ec8cbc81ec
Yutak_Seo <soano2428@gmail.com>
2023-08-07 15:01:01 +0900

    23.08.07

--- corner_detector.py
+++ corner_detector.py
@@ -16,27 +16,3 @@ def harris_corner(src, block_size, ksize, k):
     plt.show()
     
     return dst
-
-
-
-'''
-
-
-coord = np.where(corner > 0.1* corner.max())
-coord = np.stack((coord[1], coord[0]), axis=-1)
-
-for x, y in coord:
-    cv2.circle(src, (x,y), 5, (0,0,255), 1, cv2.LINE_AA)
-
-corner_norm = cv2.normalize(corner, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-
-corner_norm = cv2.cvtColor(corner_norm, cv2.COLOR_GRAY2BGR)
-#merged = np.hstack((corner_norm, src))
-
-# 이미지를 화면에 표시
-cv2.imshow('Gray Image', corner)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-#visual_array(corners)
-'''
