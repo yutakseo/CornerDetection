ddf25e5829694bc961cad0b3d029b55517b491e4
yutak <soano2428@gmail.com>
2023-08-26 19:58:32 +0900

    23.08.26 패딩 생성 코드 추가

--- createArray.py
+++ createArray.py
@@ -6,6 +6,15 @@ def make_array(data):
     matrix = np.array(data)
     return matrix
 
+def create_padding(matrix):
+    filled_matrix = matrix.copy()
+    filled_matrix[0, :] = 0  # 첫 번째 행
+    filled_matrix[-1, :] = 0  # 마지막 행
+    filled_matrix[:, 0] = 0  # 첫 번째 열
+    filled_matrix[:, -1] = 0  # 마지막 열
+    return filled_matrix
+
+
 def visual_array(title:str, data):
     matrix = np.array(data)
     rows, cols = matrix.shape
@@ -31,6 +40,6 @@ def visual_array(title:str, data):
 def binary_corner(corner_image):
     max_value = np.max(corner_image)
     binary_image = np.zeros_like(corner_image, dtype=np.uint8)
-    binary_image[corner_image == max_value] = 1
+    binary_image[corner_image == max_value] = 1 
     return binary_image
 
--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from modelEvaluation import evaluation
 
 
 
-src = test_data4
+src = truncated
 dst = harris_corner(src, 2, 3, 0.01)
 
 
-
+'''
 #코너탐색의 결과를 시각화
 visual_array("src", src)
 visual_array("harris result", dst)
@@ -25,3 +25,6 @@ visual_array("bin",binary_corner(dst))
 
 
 evaluation(test_data1_answer, binary_corner(dst))
+'''
+
+visual_array(create_padding(truncated))
